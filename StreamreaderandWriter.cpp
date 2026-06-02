d34a9ffad56da51862d43b8b6be1d5d73ffce6e8
Muh Arya Saputra <aryaom74@gmail.com>
2026-06-02 19:01:42 +0700

    Implement file writing with user input
    Add functionality to write user input to a file until '\q' is entered.

--- StreamreaderandWriter.cpp
+++ StreamreaderandWriter.cpp
@@ -4,3 +4,30 @@
 
 using namespace std;
 
+int main() 
+{
+    // Deklarasi variabel baris untuk menyimpan data string
+    string baris;
+
+    // Membuka file dalam mode menulis
+    ofstream outfile;
+    // Menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, '\\q' untuk keluar" << endl;
+
+    // Unlimited loop untuk menulis file
+    while (true) 
+    {
+        cout << "- ";
+        // Mendapat setiap karakter dalam satu baris
+        getline(cin, baris);
+
+        // Kondisi jika baris hanya berisi karakter \q untuk keluar
+        if (baris == "\\q") break;
+
+        // Menulis dan memasukkan nilai dari variabel baris ke dalam file
+        outfile << baris << endl;
+    }
+
+    
