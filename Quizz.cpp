f4f8232b08e88fa19fa76e9003c1c4ddb1dc6754
Muh Arya Saputra <aryaom74@gmail.com>
2026-06-02 19:05:49 +0700

    Update Quizz.cpp

--- Quizz.cpp
+++ Quizz.cpp
@@ -14,3 +14,18 @@ struct Barang {
     string harga;
 };
 
+class TokoElektronik {
+private:
+    // 1. Enkapsulasi: Array berkapasitas 3 elemen untuk barang di etalase
+    array<string, 3> etalase;
+    const string namaFileGudang = "gudang.txt";
+
+public:
+    // Constructor: Mengisi data awal ke etalase secara otomatis
+    TokoElektronik() {
+        etalase[0] = "Televisi LED 32 Inch";
+        etalase[1] = "Kulkas 2 Pintu";
+        etalase[2] = "Mesin Cuci Otomatis";
+    }
+
+    
