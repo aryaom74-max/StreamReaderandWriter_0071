d5482edc73c352ba882a97fb79f09428620cb8b8
Muh Arya Saputra <aryaom74@gmail.com>
2026-06-02 18:56:40 +0700

    Update ExceptionStandar.cpp

--- ExceptionStandar.cpp
+++ ExceptionStandar.cpp
@@ -8,4 +8,12 @@ int main()
 {
     cout << "Awal program" << endl; // penanda 1
     
+    try 
+    {
+        // Deklarasi array data dengan panjang index 3
+        array<int, 3> data = { 1, 2, 3 };
+        
+        // Memanggil array element ke-5 yang berarti akan terjadi error out of range
+        cout << data.at(5) << endl;
+    }
     
