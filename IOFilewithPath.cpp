28be8b1f3a48faf1d9a5b3792aad826121477a65
Muh Arya Saputra <aryaom74@gmail.com>
2026-06-02 18:58:26 +0700

    Update IOFilewithPath.cpp

--- IOFilewithPath.cpp
+++ IOFilewithPath.cpp
@@ -4,3 +4,25 @@
 
 using namespace std;
 
+int main() 
+{
+    // Deklarasi variabel baris dan NamaFile
+    string baris, NamaFile;
+
+    // Membuat input untuk menangkap nama file
+    cout << "Masukkan nama file : ";
+    cin >> NamaFile;
+
+    // Membuka file dalam mode menulis
+    ofstream outfile;
+    // Menunjuk ke sebuah nama file dengan ekstensi txt
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, '\\q' untuk keluar" << endl;
+
+    // Membersihkan sisa newline di buffer agar getline bekerja dengan benar
+    cin >> ws; 
+
+    // Looping untuk menulis isi file
+    while (true) 
+   
