3ec428152acc53bdea0f86ff5263ccba713b639e
Muh Arya Saputra <aryaom74@gmail.com>
2026-06-02 19:00:23 +0700

    Update Manualexception.cpp

--- Manualexception.cpp
+++ Manualexception.cpp
@@ -2,3 +2,16 @@
 using namespace std;
 
 int main() 
+{
+    // Membuat isi dari try untuk pengujian error
+    try 
+    {
+        cout << "Selamat belajar di prodi TI UMY" << endl;
+        
+        // Membuka comment throw agar exception dapat diuji. 
+        // Dilemparkan nilai integer (0) agar masuk ke blok catch(int a)
+        throw 0; 
+        
+        cout << "Pernyataan tidak akan ditampilkan" << endl;
+    }
+    
